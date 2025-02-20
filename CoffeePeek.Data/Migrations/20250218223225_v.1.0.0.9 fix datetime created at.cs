95d382cc3506b033a88d4613def23fb2a8c80feb
winststreloc <goloologo@gmail.com>
2025-02-20 19:38:40 +0300

    *migrations

---
+++ CoffeePeek.Data/Migrations/20250218223225_v.1.0.0.9 fix datetime created at.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeePeek.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1009fixdatetimecreatedat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
