52cb72ed8d30625bc7ea5965fa8294974b72090a
Arsent Stefanenko <stefisen@yandex.ru>
2025-03-19 10:56:41 +0300

    *fix coffeShop json property naming

--- CoffeePeek.Contract/Response/CoffeeShop/GetCoffeeShopsResponse.cs
+++ CoffeePeek.Contract/Response/CoffeeShop/GetCoffeeShopsResponse.cs
@@ -1,12 +1,18 @@
+using System.Text.Json.Serialization;
 using CoffeePeek.Contract.Dtos.CoffeeShop;
 
 namespace CoffeePeek.Contract.Response.CoffeeShop;
 
 public class GetCoffeeShopsResponse
 {
+    [JsonPropertyName("content")]
     public CoffeeShopDto[] CoffeeShopDtos { get; set; }
+    [JsonPropertyName("page")]
     public int CurrentPage { get; set; }
+    [JsonPropertyName("pageSize")]
     public int PageSize { get; set; }
+    [JsonPropertyName("totalItems")]
     public int TotalItems { get; set; }
+    [JsonPropertyName("totalPages")]
     public int TotalPages { get; set; }
 }
