cd5b5819fc17a7610dae1e0aaffa1f77dd2a38be
Arsent Stefanenko <stefisen@yandex.ru>
2025-04-07 13:24:35 +0300

    +add review service initial

---
+++ CoffeePeek.Moderation.Api/Controllers/ReviewCoffeeShopController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeePeek.Moderation.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReviewCoffeeShopController : Controller
+{
+    
+}
