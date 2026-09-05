1f25852d29f59e122867a355fcbe1b631429599c
Arseny Stefanenko <stefisen@yandex.ru>
2026-09-06 00:17:46 +0300

    fix: add missing Shops coffee focus migration

---
+++ CoffeePeek.Shops.Persistance/Migrations/20260905211420_RenameCoffeeFocusToType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeePeek.Shops.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCoffeeFocusToType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CoffeeFocus",
+                table: "Shops",
+                newName: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Shops",
+                newName: "CoffeeFocus");
+        }
+    }
+}
