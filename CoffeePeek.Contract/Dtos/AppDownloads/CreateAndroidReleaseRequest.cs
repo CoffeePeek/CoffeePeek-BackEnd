e8db3861684bdcfd96e325f8ab24336c836b3e72
Arseny Stefanenko <stefisen@yandex.ru>
2026-09-08 12:30:22 +0300

    *fix CreateAndroidReleaseRequest

--- CoffeePeek.Contract/Dtos/AppDownloads/CreateAndroidReleaseRequest.cs
+++ CoffeePeek.Contract/Dtos/AppDownloads/CreateAndroidReleaseRequest.cs
@@ -5,18 +5,26 @@ using System.ComponentModel.DataAnnotations;
 namespace CoffeePeek.Contract.Dtos.AppDownloads;
 
 public record CreateAndroidReleaseRequest(
-    [property: Required]
-    [property: MaxLength(32)]
+    [Required]
+    [MaxLength(32)]
     string Version,
+
+    [Range(1, int.MaxValue)]
     int VersionCode,
-    [property: Required]
-    [property: MaxLength(2048)]
+
+    [Required]
+    [MaxLength(2048)]
     string FileUrl,
-    [property: Required]
-    [property: MaxLength(255)]
+
+    [Required]
+    [MaxLength(255)]
     string FileName,
+
+    [Range(1, long.MaxValue)]
     long FileSize,
-    [property: Required]
-    [property: StringLength(64, MinimumLength = 64)]
+
+    [Required]
+    [StringLength(64, MinimumLength = 64)]
     string Sha256,
+
     DateTime? ReleasedAt);
